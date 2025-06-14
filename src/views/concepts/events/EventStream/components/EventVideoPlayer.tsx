d3bf3d154f74e1ba17135d9dc24c8603709a5146
Brian Kiplagat <kiplagatbrian18@gmail.com>
2025-06-14 19:20:08 +0300

    fix: autoplay

--- src/views/concepts/events/EventStream/components/EventVideoPlayer.tsx
+++ src/views/concepts/events/EventStream/components/EventVideoPlayer.tsx
@@ -3,15 +3,34 @@ import { MediaPlayer, MediaProvider, Poster } from '@vidstack/react'
 import React from 'react'
 import '@vidstack/react/player/styles/default/layouts/video.css'
 import '@vidstack/react/player/styles/default/theme.css'
+import Notification from '@/components/ui/Notification'
+
 
 import {
     defaultLayoutIcons,
     DefaultVideoLayout,
 } from '@vidstack/react/player/layouts/default'
+import { toast } from '@/components/ui'
 
 interface EventVideoPlayerProps {
     src?: string
     poster?: string
+}
+
+const onAutoPlay = () => {
+    console.log('onAutoPlay - success')
+}
+const onAutoPlayFail = () => {
+    console.log('onAutoPlayFail - failed')
+    toast.push(
+        <Notification type="danger">
+            Ops, video did not play automatically due to restriction from your
+            browser. Please press play button to start the video.
+        </Notification>,
+        {
+            placement: 'top-center',
+        },
+    )
 }
 
 const EventVideoPlayer: React.FC<EventVideoPlayerProps> = ({
@@ -22,8 +41,12 @@ const EventVideoPlayer: React.FC<EventVideoPlayerProps> = ({
         <Card className="w-full h-full p-0 m-0">
             <MediaPlayer
                 autoPlay
+                muted
                 title="..."
+                storage={src}
                 src={src}
+                onAutoPlay={onAutoPlay}
+                onAutoPlayFail={onAutoPlayFail}
                 className="w-full h-full"
             >
                 <MediaProvider className="w-full h-full">
