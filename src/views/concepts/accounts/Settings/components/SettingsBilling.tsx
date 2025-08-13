568571d3561e56d462bc0b8633e4e0b136ba4d3f
Brian Kiplagat <kiplagatbrian18@gmail.com>
2025-08-13 14:03:16 +0300

    feat: update billing settings UI to display a message when no payment methods are found and enhance layout

--- src/views/concepts/accounts/Settings/components/SettingsBilling.tsx
+++ src/views/concepts/accounts/Settings/components/SettingsBilling.tsx
@@ -6,6 +6,7 @@ import useSWR from 'swr'
 
 import type { GetSettingsBillingResponse } from '../types'
 import { apiGetStripeSubscriptions } from '@/services/AuthService'
+import FileNotFound from '@/assets/svg/FileNotFound'
 
 const SettingsBilling = () => {
     const {
@@ -27,58 +28,70 @@ const SettingsBilling = () => {
 
     return (
         <div>
-            <h4 className="mb-4">Billing</h4>
+            <h4 className="mb-4">Billing & Payments</h4>
 
             <div className="mt-8">
                 <h5>Payment method</h5>
-                <div>
-                    {data.data.cardDetails?.map((card, index) => (
-                        <div
-                            key={card.id}
-                            className={classNames(
-                                'flex items-center justify-between p-4',
-                                !isLastChild(data.data.cardDetails, index) &&
-                                    'border-b border-gray-200 dark:border-gray-600',
-                            )}
-                        >
-                            <div className="flex items-center">
-                                {card.card.brand === 'visa' && (
-                                    <img
-                                        src="/img/others/img-8.png"
-                                        alt="visa"
-                                    />
+                {data.data.cardDetails.length === 0 ? (
+                    <div className="flex flex-col items-center gap-4">
+                        <FileNotFound />
+                        <span className="font-semibold">
+                            No payment method found!
+                        </span>
+                    </div>
+                ) : (
+                    <div>
+                        {data.data.cardDetails?.map((card, index) => (
+                            <div
+                                key={card.id}
+                                className={classNames(
+                                    'flex items-center justify-between p-4',
+                                    !isLastChild(
+                                        data.data.cardDetails,
+                                        index,
+                                    ) &&
+                                        'border-b border-gray-200 dark:border-gray-600',
                                 )}
-                                {card.card.brand === 'mastercard' && (
-                                    <img
-                                        src="/img/others/img-9.png"
-                                        alt="master"
-                                    />
-                                )}
-                                <div className="ml-3 rtl:mr-3">
-                                    <div className="flex items-center">
-                                        <div className="text-gray-900 dark:text-gray-100 font-semibold">
-                                            {card.billing_details.name} ••••{' '}
-                                            {card.card.last4}
+                            >
+                                <div className="flex items-center">
+                                    {card.card.brand === 'visa' && (
+                                        <img
+                                            src="/img/others/img-8.png"
+                                            alt="visa"
+                                        />
+                                    )}
+                                    {card.card.brand === 'mastercard' && (
+                                        <img
+                                            src="/img/others/img-9.png"
+                                            alt="master"
+                                        />
+                                    )}
+                                    <div className="ml-3 rtl:mr-3">
+                                        <div className="flex items-center">
+                                            <div className="text-gray-900 dark:text-gray-100 font-semibold">
+                                                {card.billing_details.name} ••••{' '}
+                                                {card.card.last4}
+                                            </div>
                                         </div>
+                                        <span>
+                                            Expires {card.card.exp_month}/
+                                            {card.card.exp_year}
+                                        </span>
                                     </div>
-                                    <span>
-                                        Expires {card.card.exp_month}/
-                                        {card.card.exp_year}
-                                    </span>
+                                </div>
+                                <div className="flex">
+                                    <Button
+                                        size="sm"
+                                        type="button"
+                                        onClick={() => {}}
+                                    >
+                                        Edit
+                                    </Button>
                                 </div>
                             </div>
-                            <div className="flex">
-                                <Button
-                                    size="sm"
-                                    type="button"
-                                    onClick={() => {}}
-                                >
-                                    Edit
-                                </Button>
-                            </div>
-                        </div>
-                    ))}
-                </div>
+                        ))}
+                    </div>
+                )}
             </div>
 
             <div className="mt-8">
